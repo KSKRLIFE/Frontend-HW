4190ce4088bfecba8bfb08719f1aa56186b1853c
KSKRLIFE <konduru@pdx.edu>
2023-04-13 23:14:00 -0700

    HW1 - Updated
    Updated from feedback.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,10 +1,12 @@
 const elem = document.querySelector("input");
-
 elem.addEventListener("input", handleInput);
 
 function handleInput() {
   let rev = elem.value.split("").reverse().join("");
-  if (elem.value < 0 || elem.value == NaN) {
+  if (elem.value.length == 0) {
+    document.getElementById("error").style.display = "none";
+    document.getElementById("result").style.display = "none";
+  } else if (elem.value < 0 || elem.value == NaN) {
     document.getElementById("error").style.display = "block";
     document.getElementById("result").style.display = "none";
   } else if (elem.value != rev) {
