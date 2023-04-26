ed4da6469a6a8641e8369ee5ee8e5830cf25b9db
KSKRLIFE <konduru@pdx.edu>
2023-04-26 14:41:50 -0700

    HW2
    Updated HW2 Exercises.

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,17 @@
-// Add your code here
+const content = document.querySelector("#data").innerHTML;
+const input = document.querySelector("input");
 
-input.addEventListener('keydown', handleKeyDown);
+input.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown() {
+  const search = input.value;
+  let regex = new RegExp(search, "gi");
+  if (search !== "") {
+    let newText = content.replaceAll(regex, (match) => {
+      return `<span class='highlight'>${match}</span>`;
+    });
+    document.querySelector("#data").innerHTML = newText;
+  } else {
+    document.querySelector("#data").innerHTML = content;
+  }
+}
--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,41 @@
-// Add your code here
+const body = document.querySelector("#body");
+const input = document.querySelector("#input");
+const button = document.querySelector("#button");
+let intervalID = null;
+let isRunning = false;
+
+function generateRandomColorCode() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const colorCode = `rgb(${r}, ${g}, ${b})`;
+  return colorCode;
+}
+function changeBackgroundColor() {
+  const colorCode = generateRandomColorCode();
+  body.style.backgroundColor = colorCode;
+}
+
+function startBackgroundChange() {
+  if (!isRunning) {
+    if (input.value) {
+      const intervalValue = input.value;
+      intervalID = setInterval(changeBackgroundColor, intervalValue * 1000);
+      button.textContent = "Stop";
+      button.style.backgroundColor = "red";
+      isRunning = true;
+    } else {
+      intervalID = setInterval(changeBackgroundColor, 3000);
+      button.textContent = "Stop";
+      button.style.backgroundColor = "red";
+      isRunning = true;
+    }
+  } else {
+    clearInterval(intervalID);
+    button.textContent = "Start";
+    button.style.backgroundColor = "blue";
+    isRunning = false;
+  }
+}
+
+button.addEventListener("click", startBackgroundChange);
